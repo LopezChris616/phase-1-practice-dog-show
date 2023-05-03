1df959ded5ad58f8892b8ceff74c27d4a30dc03c
Christian Lopez <lopezchristian1610@gmail.com>
2023-05-03 17:31:08 -0400

    Render list of already registered dogs from db.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const tableBody = document.getElementById("table-body");
+    getDogs(tableBody);
+});
 
-})
+function getDogs(tableBody) {
+    fetch("http://localhost:3000/dogs")
+        .then(res => res.json())
+        .then(dogs => dogs.forEach(dog => displayDogs(dog, tableBody)))
+        .catch(err => console.log(err))
+}
+
+function displayDogs(dog, tableBody) {
+    const dogRow = document.createElement("tr");
+    const dogName = document.createElement("td");
+    const dogBreed = document.createElement("td");
+    const dogSex = document.createElement("td");
+    const editDog = document.createElement("button");
+
+    dogName.textContent = dog.name;
+    dogBreed.textContent = dog.breed;
+    dogSex.textContent = dog.sex;
+    editDog.textContent = "Edit Dog";
+
+    dogRow.append(dogName, dogBreed, dogSex, editDog);
+    tableBody.appendChild(dogRow);
+}
